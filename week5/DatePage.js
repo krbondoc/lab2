7bf17db633303aeb8ed6439b5ca03d97e0756527
Kylene Naiomi Bondoc <krbondoc@student.apc.edu.ph>
2023-01-15 00:06:19 +0800

    (Update) JS Datenow()

---
+++ week5/DatePage.js
@@ -0,0 +1,10 @@
+let ms = Date.now();
+
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const year = day * 365;
+
+let years = Math.round(Date.now() / year);
+
+Date.now()
