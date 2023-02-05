6211b0d557f8b7eab9a3a3ef9508c7e80a97d9f3
Kylene Naiomi Bondoc <krbondoc@student.apc.edu.ph>
2023-02-05 15:08:57 +0800

    Favicon

--- week9/main/form/form.php
+++ week9/main/form/form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <title>Feedback Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="icon" type="image/x-icon" href="wonderland.ico">
+    <link rel="icon" type="image/x-icon" href="../wonderland.ico">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
